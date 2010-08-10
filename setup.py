0f19e57a6970b22987d86da191d44dc76c1839ab
Luis Carlos Cruz <carlitos.kyo@gmail.com>
2010-08-09 23:43:17 -0400

    setup file updated

--- setup.py
+++ setup.py
@@ -2,45 +2,48 @@
 
 import sys, os
 
-__author__ = 'Ryan McGrath <ryan@venodesigns.net>'
-__version__ = '1.2'
+__author__ = 'Luis Carlos Cruz <carlitos.kyo@gmail.com>'
+__version__ = '0.1'
 
 # Distutils version
 METADATA = dict(
-	name = "twython",
-	version = __version__,
-	py_modules = ['setup', 'twython/__init__', 'twython/core', 'twython/twyauth', 'twython/streaming', 'twython/oauth'],
-	author = 'Ryan McGrath',
-	author_email = 'ryan@venodesigns.net',
-	description = 'An easy (and up to date) way to access Twitter data with Python.',
-	long_description = open("README.markdown").read(),
-	license = 'MIT License',
-	url = 'http://github.com/ryanmcgrath/twython/tree/master',
-	keywords = 'twitter search api tweet twython',
+    name = "mtweets",
+    version = __version__,
+    py_modules = ['setup',
+                  'mtweets/__init__',
+                  'mtweets/api',
+                  'mtweets/streaming'],
+    author = 'Luis Carlos Cruz',
+    author_email = 'carlitos.kyo@gmail.com',
+    description = 'An easy (and up to date) way to access Twitter data with Python.',
+    long_description = open("README.markdown").read(),
+    license = 'MIT License',
+    url = 'http://github.com/carlitux/mtweets',
+    keywords = 'twitter search api tweet twython',
 )
 
 # Setuptools version
 SETUPTOOLS_METADATA = dict(
-	install_requires = ['setuptools', 'simplejson'],
-	include_package_data = True,
-	classifiers = [
-		'Development Status :: 4 - Beta',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: MIT License',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		'Topic :: Communications :: Chat',
-		'Topic :: Internet',
-	]
+    install_requires = ['setuptools', 'simplejson', 'oauth'],
+    include_package_data = True,
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Communications :: Chat',
+        'Topic :: Internet',
+    ]
 )
 
 def Main():
-	try:
-		import setuptools
-		METADATA.update(SETUPTOOLS_METADATA)
-		setuptools.setup(**METADATA)
-	except ImportError:
-		import distutils.core
-		distutils.core.setup(**METADATA)
+    try:
+        import setuptools
+        METADATA.update(SETUPTOOLS_METADATA)
+        setuptools.setup(**METADATA)
+    except ImportError:
+        import distutils.core
+        distutils.core.setup(**METADATA)
 
 if __name__ == '__main__':
-  Main()
+    Main()
